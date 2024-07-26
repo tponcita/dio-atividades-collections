803653df66dc6b544b71b0705412d81351db8312
Thalita Ponce <thalitaponce2009@hotmail.com>
2024-07-25 23:27:51 -0300

    Exercicios de Listas de livro e pessoa

---
+++ collections-java-api-2023/src/main/java/list/pesquisa/CatalogoLivros.java
@@ -0,0 +1,73 @@
+package main.java.list.pesquisa;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class CatalogoLivros {
+    //atributo
+    private List<Livro> livroList;
+
+    public CatalogoLivros() {
+        this.livroList = new ArrayList<>();
+    }
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao){
+        this.livroList.add(new Livro(titulo, autor, anoPublicacao));
+    }
+
+    public List<Livro> pesquisarPorAutor(String autor){
+        List<Livro> livrosPorAutor = new ArrayList<>();
+        if(!livroList.isEmpty()){
+            for(Livro l :  livroList){
+                if(l.getAutor().equalsIgnoreCase(autor)){
+                    livrosPorAutor.add(l);
+                }
+            }
+        }
+        return livrosPorAutor;
+    }
+
+    public List<Livro>pesquisarPorIntervaloAnos(int anoInicial, int anoFinal){
+        List<Livro> livrosPorIntervaloAnos = new ArrayList();
+        if (!this.livroList.isEmpty()) {
+            Iterator var4 = this.livroList.iterator();
+
+            while(var4.hasNext()) {
+                Livro l = (Livro)var4.next();
+                if (l.getAnoPublicacao() >= anoInicial && l.getAnoPublicacao() <= anoFinal) {
+                    livrosPorIntervaloAnos.add(l);
+                }
+            }
+
+            return livrosPorIntervaloAnos;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+    public Livro pesquisarPorTitulo(String titulo){
+        Livro livroPorTitulo =  null;
+        if(!livroList.isEmpty()){
+            for(Livro l : livroList){
+                if(l.getTitulo().equalsIgnoreCase(titulo)){
+                    livroPorTitulo = l;
+                    break;
+                }
+            }
+        }
+        return livroPorTitulo;
+    }
+
+    public static void main(String[] args) {
+        CatalogoLivros catalogoLivros = new CatalogoLivros();
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 1", 2021);
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 2", 2020);
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 2", 2022);
+        catalogoLivros.adicionarLivro("Livro 2", "Autor 3", 2023);
+        catalogoLivros.adicionarLivro("Livro 3", "Autor 4", 1994);
+
+        System.out.println(catalogoLivros.pesquisarPorAutor("Autor 2"));
+        System.out.println(catalogoLivros.pesquisarPorIntervaloAnos(2020, 2021));
+        System.out.println(catalogoLivros.pesquisarPorTitulo("Livro 1"));
+    }
+}
